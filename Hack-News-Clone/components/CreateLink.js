22560e66b8e3b0aa1d836693fade87e08801273c
E-Loubbidi <48106590+E-Loubbidi@users.noreply.github.com>
2021-04-12 08:55:33 +0000

    Add files via upload

---
+++ Hack-News-Clone/components/CreateLink.js
@@ -0,0 +1,90 @@
+import { gql, useMutation } from '@apollo/client';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router';
+import { LINKS_PER_PAGE } from '../constants';
+import { FEED_QUERY } from './LinkList';
+
+const CreateLink = () => {
+    const [formState, setFormState] = useState({
+        description: '',
+        url: ''
+    });
+
+    const CREATE_LINK_MUTATION = gql`
+        mutation PostMutation(
+            $description: String!
+            $url: String!) {
+                post(description: $description, url: $url) {
+                        id
+                        createdAt
+                        url
+                        description
+                    }
+            }
+    `;
+
+    const history = useHistory();
+
+    const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+        variables: {
+            description: formState.description,
+            url: formState.url
+        },
+        update: (cache, {data: {post}}) => {
+            const take = LINKS_PER_PAGE;
+            const skip = 0;
+            const orderBy = { createdAt: 'desc' };
+
+            const data = cache.readQuery({
+                query: FEED_QUERY,
+                variables: {
+                    take,
+                    skip,
+                    orderBy
+                }
+            });
+            
+            cache.writeQuery({
+                query: FEED_QUERY,
+                data: {
+                    feed: {
+                        links: [post, ...data.feed.links]
+                    }
+                },
+                variables: {
+                    take,
+                    skip,
+                    orderBy
+                }
+            });
+        },
+        onCompleted: () => history.push('/new/1')
+    });
+
+    return (
+        <div>
+            <form onSubmit={e => {
+                e.preventDefault();
+                createLink();
+            }}>
+                <div className="flex flex-column mt3">
+                    <input
+                    className="mb2" 
+                    type="text" 
+                    value={formState.desription} 
+                    onChange={e => setFormState({ ...formState, description: e.target.value })} 
+                    placeholder="A description for the link" />
+                    <input
+                    className="mb2" 
+                    type="text" 
+                    value={formState.url} 
+                    onChange={e => setFormState({ ...formState, url: e.target.value })} 
+                    placeholder="The URL for the link" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>        
+        </div>
+    )
+}
+
+export default CreateLink
